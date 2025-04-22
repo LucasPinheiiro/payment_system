6e348ba070d75d05e875c7afcd131f1b0ac36260
LucasPinheiro <apinheiro.lucas@gmail.com>
2025-04-22 17:23:52 -0300

    Initial commit from springboot to folder only.

---
+++ pixpayment/src/main/java/com/lucaspinheiro/pixpayment/PixPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lucaspinheiro.pixpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ pixpayment/src/test/java/com/lucaspinheiro/pixpayment/PixPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaspinheiro.pixpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
